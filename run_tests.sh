86cbe9bb52a1ef47aca44d5a31eaa7953b4fdc39
cupid5trick <50273795+cupid5trick@users.noreply.github.com>
2025-07-26 20:13:40 +0800

    Fix ci workflow

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 test_log=test.$(date +'%Y%m%dT%H%M%S').log
-python test.py && python -m unittest |& tes $test_log
+(poetry run -- python test.py && python -m unittest) |& tee $test_log
